a8884ef84d0d8b2dc56f5d8217aad6ed0c010b60
Yuki Tsuji <33805106+coils@users.noreply.github.com>
2018-04-05 20:39:46 +0900

    Add files via upload

---
+++ require_once/functions.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * functions.php
+ *
+ * 関数
+ * 
+ * @copyright (c) 2018 Yuki Tsuji
+ */
+
+/**
+ * XSS対策関数
+ * @param string $string
+ * @return string
+ */
+function h($string) {
+    return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+}
+
+/**
+ * ページング
+ * @param int $page
+ * @param int $total
+ * @return string
+ */
+function paging($page, $total) {
+    $delta = 3;
+    if ($total < 1) {
+        return;
+    }
+    $query = (is_array(filter_input_array(INPUT_GET))) ?
+        filter_input_array(INPUT_GET) : [];
+    if (isset($query['offset_page'])) {
+        unset($query['offset_page']);
+    }
+    $querystring = http_build_query($query);
+    $limit = 10;
+    $placeholder = "<span%s><a href=\"?offset_page=%d&%s\">%s</a></span> ";
+
+    // 最大ページ数
+    $maxPage = ceil($total / $limit);
+
+    $min = max([$page - $delta, 1]);
+    $max = min([$page + $delta, $maxPage - 1]);
+
+    $html = '';
+    if ($page > 1) {
+        $html .= sprintf($placeholder, '', 1, $querystring, '&laquo;');
+        $html .= sprintf($placeholder, '', $page, $querystring, '前へ');
+    }
+    for ($i = $max - 6; $i < $min + 7; $i++) {
+        if ($i > -1 && $i < $maxPage) {
+            $html .= sprintf($placeholder, ($i == $page - 1) ? ' class="active"' : '', $i + 1, $querystring, $i + 1);
+        }
+    }
+    if ($page < $maxPage) {
+        $html .= sprintf($placeholder, '', $page + 1, $querystring, '次へ');
+        $html .= sprintf($placeholder, '', $maxPage, $querystring, '&raquo;');
+    }
+    return $html;
+}
+?>
