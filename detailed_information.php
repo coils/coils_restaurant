814c241c3302b44d1dc3c7094ea3de6170fc6bd4
Yuki Tsuji <33805106+coils@users.noreply.github.com>
2018-05-21 22:38:39 +0900

    Update detailed_information.php

--- detailed_information.php
+++ detailed_information.php
@@ -36,8 +36,8 @@ $restaurants = Gnavi_api::getRestaurants();
             <div>
                 <?php if (isset($restaurants->error)): ?>
                     <div>
-                        <p><?php echo $restaurants->error->code; ?></p>
-                        <p><?php echo $restaurants->error->message; ?></p>
+                        <p><?php echo h($restaurants->error->code); ?></p>
+                        <p><?php echo h($restaurants->error->message); ?></p>
                     </div>
                 <?php elseif ($restaurants->total_hit_count == 1): ?>
                     <table class="type2" align="center">
@@ -46,10 +46,16 @@ $restaurants = Gnavi_api::getRestaurants();
                         </tr>
                         <tr>
                             <td>
-                                <div style="text-align: center;">
-                                    <img src="<?php echo h($restaurants->rest->image_url->shop_image1); ?>">
-                                    <img src="<?php echo h($restaurants->rest->image_url->shop_image2); ?>">
-                                </div>
+                                <?php if (mb_strlen($restaurants->rest->image_url->shop_image1) >= 1 || mb_strlen($restaurants->rest->image_url->shop_image2) >= 1): ?>
+                                    <div style="text-align: center;">
+                                        <img src="<?php echo h($restaurants->rest->image_url->shop_image1); ?>">
+                                        <img src="<?php echo h($restaurants->rest->image_url->shop_image2); ?>">
+                                    </div>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
                             </td>
                         </tr>
                         <tr>
@@ -57,68 +63,144 @@ $restaurants = Gnavi_api::getRestaurants();
                         </tr>
                         <tr>
                             <td>
-                                <?php echo h($restaurants->rest->name); ?><br />
-                                <a href="<?php echo h($restaurants->rest->url); ?>">ぐるなび店舗ページ</a>
+                                <?php if (mb_strlen($restaurants->rest->name) >= 1): ?>
+                                    <?php echo h($restaurants->rest->name); ?><br />
+                                    <a href="<?php echo h($restaurants->rest->url); ?>">ぐるなび店舗ページはこちら</a>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
                             </td>
                         </tr>
                         <tr>
                             <th>店舗名(カタカナ)</th>
                         </tr>
                         <tr>
-                            <td><?php echo h($restaurants->rest->name_kana); ?></td>
+                            <td>
+                                <?php if (mb_strlen($restaurants->rest->name_kana) >= 1): ?>
+                                    <?php echo h($restaurants->rest->name_kana); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                         <tr>
                             <th>カテゴリー</th>
                         </tr>
                         <tr>
-                            <td><?php echo h($restaurants->rest->category); ?></td>
+                            <td>
+                                <?php if (mb_strlen($restaurants->rest->category) >= 1): ?>
+                                    <?php echo h($restaurants->rest->category); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                         <tr>
                             <th>PR文</th>
                         </tr>
                         <tr>
-                            <td><?php echo h($restaurants->rest->pr->pr_short); ?></td>
+                            <td>
+                                <?php if (mb_strlen($restaurants->rest->pr->pr_short) >= 1): ?>
+                                    <?php echo h($restaurants->rest->pr->pr_short); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                         <tr>
                             <th>営業時間</th>
                         </tr>
                         <tr>
-                            <td><?php echo h($restaurants->rest->opentime); ?></td>
+                            <td>
+                                <?php if (mb_strlen($restaurants->rest->opentime) >= 1): ?>
+                                    <?php echo h($restaurants->rest->opentime); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                         <tr>
                             <th>休業日</th>
                         </tr>
                         <tr>
-                            <td><?php echo h($restaurants->rest->holiday); ?></td>
+                            <td>
+                                <?php if (mb_strlen($restaurants->rest->holiday) >= 1): ?>
+                                    <?php echo h($restaurants->rest->holiday); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                         <tr>
                             <th>平均予算(円)</th>
                         </tr>
                         <tr>
-                            <td><?php echo h($restaurants->rest->budget); ?></td>
+                            <td>
+                                <?php if (mb_strlen($restaurants->rest->budget) >= 1): ?>
+                                    <?php echo h($restaurants->rest->budget); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                         <tr>
                             <th>電話番号</th>
                         </tr>
                         <tr>
-                            <td><?php echo h($restaurants->rest->tel); ?></td>
+                            <td>
+                                <?php if (mb_strlen($restaurants->rest->tel) >= 1): ?>
+                                    <?php echo h($restaurants->rest->tel); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                         <tr>
                             <th>アクセス</th>
                         </tr>
                         <tr>
                             <td>
-                                <?php echo h($restaurants->rest->access->line); ?><br />
-                                <?php echo h($restaurants->rest->access->station); ?><br />
-                                <?php echo h($restaurants->rest->access->walk); ?>分<br />
-                                <?php echo h($restaurants->rest->access->note); ?>
+                                <?php if (mb_strlen($restaurants->rest->access->line) >= 1): ?>
+                                    <?php echo h($restaurants->rest->access->line); ?><br />
+                                    <?php echo h($restaurants->rest->access->station); ?><br />
+                                    <?php echo h($restaurants->rest->access->walk); ?>分<br />
+                                    <?php echo h($restaurants->rest->access->note); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
                             </td>
                         </tr>
                         <tr>
                             <th>住所</th>
                         </tr>
                         <tr>
-                            <td><?php echo h($restaurants->rest->address); ?></td>
+                            <td>
+                                <?php if (mb_strlen($restaurants->rest->address) >= 1): ?>
+                                    <?php echo h($restaurants->rest->address); ?>
+                                <?php else: ?>
+                                    <div class="gray_font">
+                                        <?php echo h('データが存在しません．'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                     </table>
                 <?php endif; ?>
