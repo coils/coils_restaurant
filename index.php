436e6eab25f7d171f1038bc56d70026f61e5daa3
Yuki Tsuji <33805106+coils@users.noreply.github.com>
2018-05-21 21:48:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
  * @copyright (c) 2018 Yuki Tsuji
  */
 
+require_once './require_once/session.php';
 require_once './require_once/functions.php';
 ?>
 <!DOCTYPE html>
@@ -40,7 +41,7 @@ require_once './require_once/functions.php';
                                 alert("位置情報の利用が許可されていません．");
                                 break;
                             case 2: // POSITION_UNAVAILABLE
-                                alert("現在位置が取得出来ませんでした．");
+                                alert("現在位置が取得できませんでした．");
                                 break;
                             case 3: // TIMEOUT
                                 alert("タイムアウトになりました．");
@@ -63,18 +64,22 @@ require_once './require_once/functions.php';
             <p>
                 Restoracioは，レストラン情報を検索することが出来るWebアプリケーションです．
             </p>
-            <p>
-                検索結果は，1000件までしか表示することが出来ません．
-            </p>   
-            <p>
-                位置情報取得ボタンを押すと，緯度と経度のフォームに現在位置の座標が入力されます．
-            </p>
-            <div style="text-align: center;">
-                <button onclick="getPosition();" class="gray_button">位置情報取得</button>
-            </div>
             </fieldset>
 
             <br />
+
+            <?php if ($_SESSION['search_criteria'] === 'search_criteria'): ?>
+                <fieldset>
+                <legend>位置情報取得</legend>
+                <p>
+                    位置情報取得ボタンを押すと，緯度と経度のフォームに現在位置の座標が入力されます．
+                </p>
+                <div style="text-align: center;">
+                    <button onclick="getPosition();" class="gray_button">位置情報取得</button>
+                </div>
+                </fieldset>
+                <br />
+            <?php endif; ?>
 
             <fieldset>
             <legend>検索条件入力フォーム</legend>
@@ -84,26 +89,41 @@ require_once './require_once/functions.php';
                         <label for="freeword">検索ワード</label><br />
                         <input type="text" name="freeword" id="freeword" class="form" size="50" maxlength="100" value="<?php echo h(filter_input(INPUT_GET, 'freeword')); ?>">
                     </p>
+                    <?php if ($_SESSION['search_criteria'] === 'search_criteria'): ?>
+                        <p>
+                            <label for="latitude">緯度</label><br />
+                            <input type="text" name="latitude" id="latitude" class="form" size="50" maxlength="100" value="<?php echo h(filter_input(INPUT_GET, 'latitude')); ?>">
+                        </p>
+                        <p>
+                            <label for="longitude">経度</label><br />
+                            <input type="text" name="longitude" id="longitude" class="form" size="50" maxlength="100" value="<?php echo h(filter_input(INPUT_GET, 'longitude')); ?>">
+                        </p>
+                        <p>
+                            <label for="range">範囲(半径)</label><br />
+                            <select id="range" name="range">
+                                <option value="1">300m</option>
+                                <option value="2">500m</option>
+                                <option value="3" selected>1km</option>
+                                <option value="4">2km</option>
+                                <option value="5">3km</option>
+                            </select>
+                        </p>
+                    <?php endif; ?>
                     <p>
-                        <label for="latitude">緯度</label><br />
-                        <input type="text" name="latitude" id="latitude" class="form" size="50" maxlength="100" value="<?php echo h(filter_input(INPUT_GET, 'latitude')); ?>">
+                        <button type="submit" class="gray_button">検索</button>
                     </p>
-                    <p>
-                        <label for="longitude">経度</label><br />
-                        <input type="text" name="longitude" id="longitude" class="form" size="50" maxlength="100" value="<?php echo h(filter_input(INPUT_GET, 'longitude')); ?>">
-                    </p>
-                    <p>
-                        <label for="range">範囲(半径)</label><br />
-                        <select id="range" name="range">
-                            <option value="1">300m</option>
-                            <option value="2">500m</option>
-                            <option value="3" selected>1km</option>
-                            <option value="4">2km</option>
-                            <option value="5">3km</option>
-                        </select>
-                    </p>
-                    <button type="submit" class="gray_button">検索</button>
                 </form>
+                <?php if ($_SESSION['search_criteria'] === 'search_criteria'): ?>
+                    <form action="" name="back_search_criteria" method="POST">
+                        <input type="hidden" name="back_search_criteria" value="">
+                        <a href="javascript:back_search_criteria.submit()">元に戻す</a>
+                    </form>
+                <?php else: ?>
+                    <form action="" name="search_criteria" method="POST">
+                        <input type="hidden" name="search_criteria" value="">
+                        <a href="javascript:search_criteria.submit()">詳細な検索条件</a>
+                    </form>
+                <?php endif; ?>
             </div>
             </fieldset>
 
